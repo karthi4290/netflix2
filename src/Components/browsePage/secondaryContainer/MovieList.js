0f811bfb0cf1fbdeaea68d8475ad42e8ac7f4a90
karthikreddy.mudireddy <karthikreddy.mudireddy@incentsoft.com>
2024-03-03 12:54:49 -0600

    added navigation

--- src/Components/browsePage/secondaryContainer/MovieList.js
+++ src/Components/browsePage/secondaryContainer/MovieList.js
@@ -1,17 +1,57 @@
+import { useEffect, useRef, useState } from 'react';
 import Moviecard from './Moviecard';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-const MovieList = ({ movies,title }) => {
+const MovieList = ({ movies, title }) => {
+  const [isScrollLeft, setIsScrollLeft] = useState(false);
+  const [isScrollRight, setIsScrollRight] = useState(true);
+  const sliderRef = useRef(null);
 
+  useEffect(() => {
+    const sliderCurrentRef = sliderRef.current;
+    const handleScroll = () => {
+      setIsScrollLeft(sliderCurrentRef.scrollLeft > 0);
+      setIsScrollRight(sliderCurrentRef.scrollLeft + sliderCurrentRef.offsetWidth < sliderCurrentRef.scrollWidth);
+    }
+
+    sliderCurrentRef.addEventListener('scroll', handleScroll);
+
+    return (() => sliderCurrentRef.removeEventListener('scroll', handleScroll));
+  }, [])
+
+
+
+
+  const handleNavigation = (direction) => {
+    const sliderCurrentRef = sliderRef.current;
+    const scrollAmount = sliderCurrentRef.offsetWidth;
+    console.log(scrollAmount)
+    if (direction === "left") {
+      sliderCurrentRef.scrollLeft -= scrollAmount;
+    } else {
+      sliderCurrentRef.scrollLeft += scrollAmount;
+    }
+  }
 
   return (
-    <div className="p-6 relative ">
+    <div className="p-5 relative ">
       <h1 className="text-lg md:text-3xl py-4 text-white">{title}</h1>
-      <div id="slider" className="w-full h-full  overflow-x-hidden overscroll-x-scroll scroll whitespace-nowrap scroll-smooth relative">
+      {isScrollLeft && <MdChevronLeft
+        onClick={() => handleNavigation('left')}
+        className="cursor-pointer bg-gray-300 opacity-50 hover:opacity-100 absolute top-1/2 left-0 transform -translate-y-1/2 z-10"
+        size={40}
+      />}
+      <div ref={sliderRef} id="slider" className="w-full h-full  overflow-x-hidden overscroll-x-scroll scroll whitespace-nowrap scroll-smooth relative">
         {movies?.map((movie) => (
           <Moviecard key={movie.id} posterPath={movie.poster_path} />
         ))}
       </div>
-      
+      {isScrollRight && <MdChevronRight
+        onClick={() => handleNavigation('right')}
+        className="cursor-pointer bg-gray-300 opacity-50 hover:opacity-100 absolute top-1/2 right-3 transform -translate-y-1/2 z-10"
+        size={40}
+      />}
+
     </div>
   )
 }
--- src/Components/browsePage/secondaryContainer/SecondaryContainer.js
+++ src/Components/browsePage/secondaryContainer/SecondaryContainer.js
@@ -6,7 +6,7 @@ const SecondaryContainer = () => {
   if (!nowPlayingMovies) return
   return (
     <div className="bg-black">
-      <div className="-mt-44  pl-4 md:pl-12 relative z-20">
+      <div className="-mt-36  pl-4 md:pl-8 relative ">
         <MovieList title={"Now Playing"} movies={nowPlayingMovies}/>
         <MovieList title={"Popular"} movies={nowPlayingMovies}/>
         <MovieList title={"Upcoming"} movies={nowPlayingMovies}/>
